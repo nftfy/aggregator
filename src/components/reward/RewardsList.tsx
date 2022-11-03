bc1b1f8b4d6491b6655f705d0bb11b1f506def81
wellington fernandes <wellingtonfds@gmail.com>
2022-11-03 18:47:24 -0300

    create reward card

---
+++ src/components/reward/RewardsList.tsx
@@ -0,0 +1,48 @@
+
+import banner from 'public/banner-example.png';
+import collection from 'public/collection-example.png';
+import styled from 'styled-components';
+import tx from 'tailwind-styled-components';
+import { CardReward } from './CardReward';
+
+export const RewardsList = () => {
+  return (
+    <CardReward banner={banner.src} collectionImage={collection.src} />
+  )
+}
+
+const { CardTw, ContentTw } = {
+  CardTw: tx.div`
+    flex-auto
+    h-365
+    rounded-2xl
+    border
+    border-zinc-400
+    shadow-sm
+    overflow-hidden
+  `,
+  ContentTw:tx.div`
+    flex 
+    flex-col
+    p-4
+    gap-2
+  `
+}
+const {Card, Header, Content, HideContent, Banner} = {
+  Card: styled(CardTw)`
+    max-width: 276px;
+    height: 359px;
+  `,
+  Header: styled.div<{hidden:boolean}>`
+    position: relative;
+    overflow: hidden;
+    box-sizing: border-box;
+    display: block;
+    overflow: hidden;
+    height: ${props => props.hidden ? 0 : 155}px;
+    transition: height .3s ease-in-out;
+  `,
+  Banner: styled.img``,
+  HideContent: styled.div``,
+  Content: styled(ContentTw)``
+}
