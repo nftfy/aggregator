2ae3db4a0a8a2d265140de295bcd9cdbd8281840
wellington fernandes <wellingtonfds@gmail.com>
2022-11-16 15:11:02 -0300

    create a new header collection

--- pages/collections/[id].tsx
+++ pages/collections/[id].tsx
@@ -5,7 +5,6 @@ import AttributesFlex from 'components/AttributesFlex'
 import ExploreFlex from 'components/ExploreFlex'
 import ExploreTokens from 'components/ExploreTokens'
 import FormatEth from 'components/FormatEth'
-import Hero from 'components/Hero'
 import Layout from 'components/Layout'
 import Sidebar from 'components/Sidebar'
 import SortMenuExplore from 'components/SortMenuExplore'
@@ -31,6 +30,7 @@ import { useState } from 'react'
 import { FiRefreshCcw } from 'react-icons/fi'
 import { RewardPools } from '../../src/components/reward/pools/RewardPools'
 import SpecificPublicTable from '../../src/components/rockpool/SpecificPublicTable'
+import { BannerNfty } from '../../src/components/shared/layout/BannerNfty'
 
 // Environment variables
 // For more information about these variables
@@ -73,7 +73,7 @@ const Home: NextPage<Props> = ({ fallback, id }) => {
     collectionResponse.data && collectionResponse.data[0]
       ? collectionResponse.data[0]
       : undefined
-
+  
   const stats = useCollectionStats(router, id)
 
   const { tokens, ref: refTokens } = useTokens(
@@ -187,7 +187,7 @@ const Home: NextPage<Props> = ({ fallback, id }) => {
           {description}
           {image}
         </Head>
-        <Hero collectionId={id} fallback={fallback} />
+        {id && <BannerNfty collectionId={id} chainId={Number(CHAIN_ID)} />}
         <Tabs.Root
           value={router.query?.tab?.toString() || 'items'}
           className="col-span-full grid grid-cols-4 gap-x-4 md:grid-cols-8 lg:grid-cols-12 3xl:grid-cols-16 4xl:grid-cols-21"
