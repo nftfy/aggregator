323057cc56de6e6cc702a6c9bc1442285d1b0c18
wellington fernandes <wellingtonfds@gmail.com>
2022-11-22 17:05:58 -0300

    fix bug unlock erc721

--- src/hook/reward/erc721/useErc721SetApprovalForAll.tsx
+++ src/hook/reward/erc721/useErc721SetApprovalForAll.tsx
@@ -1,7 +1,10 @@
+import { useEffect, useState } from 'react'
 import { usePrepareContractWrite, useSendTransaction } from 'wagmi'
 import { erc721Abi } from '../../../contracts/erc721/Erc721Abi'
+import { useObserverTransaction } from '../../shared/useObserveTransaction'
 
 export const useErc721SetApprovalForAll = (contractAddress: string, spenderAddress: string) => {
+  const [isExecuting, setIsExecuting] = useState(false)
   const { config } = usePrepareContractWrite({
     addressOrName: contractAddress,
     contractInterface: erc721Abi,
@@ -9,11 +12,24 @@ export const useErc721SetApprovalForAll = (contractAddress: string, spenderAddre
     args: [spenderAddress, true]
   })
 
-  const { sendTransaction, isLoading, isSuccess } = useSendTransaction(config)
+  const { sendTransaction, isSuccess, data } = useSendTransaction(config)
+  const { isLoading: isLoading, status, dismiss } = useObserverTransaction(data, isSuccess)
+
+  useEffect(() => {
+    if (!isLoading && status === 'success') {
+      setIsExecuting(false)
+      dismiss()
+    }
+  }, [dismiss, isLoading, status])
 
   return {
-    isLoading: isLoading,
-    setApprovalForAll: sendTransaction,
+    isLoading: isLoading || isExecuting,
+    setApprovalForAll: () => {
+      if (sendTransaction) {
+        setIsExecuting(true)
+        sendTransaction()
+      }
+    },
     status: isSuccess
   }
 }
